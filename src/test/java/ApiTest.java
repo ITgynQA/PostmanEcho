e5e9c5b5f7908617732fa8c9a8d8f9d90133b85b
Ekaterina Evdokimova <kate_evd79@mail.ru>
2023-05-19 10:43:03 +0400

    Modified ApiTest

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -15,7 +15,7 @@ public class ApiTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("sume data"));
 
     }
 }
